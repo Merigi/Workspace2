50240cec17f3bcd6b1afa00eb096d2612b47b3b9
Merigi <kmerigi@gmail.com>
2014-06-20 14:41:46 +0300

    I have submitted all the code from my tango-with-django project.
    I have been working on this project from some time and this the first time am using git to manage this code.

---
+++ tango_with_django_project/rango/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from rango.models import Category, Page, UserProfile
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url', 'views')
+
+class CatAdmin(admin.ModelAdmin):
+	list_display = ('name', 'views', 'likes')
+	
+admin.site.register(Category, CatAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
+
